a07c55bc9d17084622ec01d2e721bf34de54af3d
Luis Morales <luismorales@Luiss-MacBook-Air.local>
2022-10-29 16:02:47 +0100

    update

--- lib/component/feed.dart
+++ lib/component/feed.dart
@@ -1,5 +1,25 @@
 import 'package:flutter/material.dart';
 
+class CustomCard extends StatelessWidget {
+  const CustomCard({
+    super.key,
+    required this.index,
+  });
+
+  final int index;
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+      height: 300,
+      width: 300,
+      child: Card(
+        child: Text("Each new will go here")
+        )
+      );
+
+  }
+}
 class TabFeed extends StatelessWidget {
   const TabFeed({required super.key});
   // Fields in a Widget subclass are always marked "final".
@@ -8,14 +28,28 @@ class TabFeed extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-
       height: 36.0, // in logical pixels
       padding: const EdgeInsets.symmetric(horizontal: 8.0),
       decoration: BoxDecoration(color: Colors.blue[200]),
       // Row is a horizontal, linear layout.
-      child: Row(
-        children: const [],
+      child: GridView.count(
+          crossAxisCount: 1,
+          // Images fetched from the users mobile storage would go below in the arry.
+          children: [
+            CustomCard(
+                index:1
+            ),
+            CustomCard(
+                index:1
+            ),
+            CustomCard(
+                index:1
+            )
+
+          ]
       ),
     );
+
   }
 }
+
